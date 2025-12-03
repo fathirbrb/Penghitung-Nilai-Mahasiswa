b4dc0ad5e7a3a3ea551813e39670d808a8e832ed
SandiZulian <zuliansyahsandi4@gmail.com>
2025-12-03 16:59:21 +0700

    Update penilaian_mahasiswa.sql

--- penilaian_mahasiswa.sql
+++ penilaian_mahasiswa.sql
@@ -24,8 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `absensi`
---
+-- Table structure for table `absensi`INSERT INTO `admins` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+
+CREATE TABLE `admins` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `admins` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` int NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 12345, 'user');
 
 CREATE TABLE `absensi` (
   `id_absensi` int NOT NULL,
@@ -5659,6 +5678,7 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
 
 
 
